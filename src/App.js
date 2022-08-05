ebf2b4fa972329fe3ff36001a7f5124722fbcdaf
Michael Russell <michael.russell@passivuk.com>
2022-08-05 17:40:56 +0100

    first version with rows

--- src/App.js
+++ src/App.js
@@ -11,16 +11,53 @@ class App extends Component {
     return (
       <div className="App">
         <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-              Guardians of the Galaxy
-          </p>
-          <a  className="App-link" href="https://5bd5x3swz7ch4uxvln2bqscwze0gltjb.lambda-url.eu-west-2.on.aws/"> Yay!</a>
+            <header className="App-header">
+	    <Devices />
         </header>
       </div>
     );
   }
 }
 
+class Devices extends Component{
+  render() {
+    return (
+	    <div className="App-table">
+	    <h3> Guardians of the Galaxy </h3>
+	    <DeviceRows/>
+	    <div>
+	    <a className="App-link" href="https://5bd5x3swz7ch4uxvln2bqscwze0gltjb.lambda-url.eu-west-2.on.aws/"> Download your data!</a>
+	    </div>
+
+	</div>
+    );
+  }  	    
+}
+
+class Spinner extends Component{
+  render() {
+    return (
+	    <div>
+            <img src={logo} className="App-logo-small" alt="logo" />
+	    <span> {this.props.text}</span>
+            </div>
+
+    );
+  }
+}
+
+class DeviceRows extends Component {
+  render() {
+    return (<div>
+	                <hr/>
+	    <div className="App-table-row">
+
+	    <Spinner text="Detecting devices..."/>
+	    </div>
+
+            <hr/>
+	    </div>
+    );
+  }
+}
 export default withAuthenticator(App);
