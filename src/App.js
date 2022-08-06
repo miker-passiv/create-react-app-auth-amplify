29d746c8949939119a64c470cbdf9693e731b0ea
Michael Russell <michael.russell@passivuk.com>
2022-08-06 13:07:40 +0100

    ui updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+import Dygraph from 'dygraphs';
+import 'dygraphs/dist/dygraph.min.css'
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -12,52 +16,136 @@ class App extends Component {
       <div className="App">
         <AmplifySignOut />
             <header className="App-header">
-	    <Devices />
+	    <DevicesList />
+
         </header>
       </div>
     );
   }
+  componentDidMount() {
+      let timer=setInterval(getData(),10000);
+  }
 }
 
-class Devices extends Component{
-  render() {
+function DevicesList(props) {
     return (
 	    <div className="App-table">
 	    <h3> Guardians of the Galaxy </h3>
 	    <DeviceRows/>
-	    <div>
-	    <a className="App-link" href="https://5bd5x3swz7ch4uxvln2bqscwze0gltjb.lambda-url.eu-west-2.on.aws/"> Download your data!</a>
-	    </div>
-
-	</div>
+   	    </div>
     );
-  }  	    
 }
 
-class Spinner extends Component{
-  render() {
+function Spinner(props) {
     return (
-	    <div>
+            <div>
             <img src={logo} className="App-logo-small" alt="logo" />
-	    <span> {this.props.text}</span>
+            <span> {props.text}</span>
             </div>
 
     );
-  }
 }
 
-class DeviceRows extends Component {
-  render() {
-    return (<div>
-	                <hr/>
-	    <div className="App-table-row">
+function DeviceDetails(props) {
+    return (
+            <div>
+            <img src={logo} className="App-logo-small" alt="logo" />
+            <span> {props.text}</span>
+            </div>
 
-	    <Spinner text="Detecting devices..."/>
+    );
+}
+
+function DeviceData(props) {
+    return (<div>
+	    <div>{"Detected Device:"+props.data.device_id.toString()}</div>
+	    <Spinner text={"Status: Operational ..."}/>
+	    <a className="App-link" href={props.data.device_events}>Data Stream Download!</a>
+	    <Graph/>
+	    <DyGraph/>
+	    </div>
+
+
+    );
+}
+
+function DeviceRows(props) {
+    return (<div>
+	    <hr/>
+	    <div className="App-table-row" id="device_table">
+	    <DeviceDetails text="Detecting devices..."/>
 	    </div>
 
             <hr/>
 	    </div>
     );
+}
+
+function Graph(props){
+    return (<div>---</div>)
+}
+
+
+
+class DyGraph extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div id="graph"></div>;
+  }
+
+  componentDidMount() {
+    const g = new Dygraph('graph',  `
+    '2016/01/01',10,20
+    '2016/07/01',20,10
+    '2016/12/31',40,30
+    '2017/07/01',20,10
+    '2017/12/31',40,30
+    `, {
+        title: 'Environmental Trends',
+        titleHeight: 32,
+        ylabel: 'Absolute Valeus',
+        xlabel: 'Time',
+        gridLineWidth: '0.1',
+        width: 700,
+        height: 300,
+        connectSeparatedPoints: true,
+	strokeWidth: 3,
+        highlightCircleSize: 10,
+        axes: { "x": { "axisLabelFontSize": 9 }, "y": { "axisLabelFontSize": 9 } },
+        labels: ['Date', 'Photonic Accumulation','Phononic Accumulation'],
+
+          colors: ["rgb(51,204,204)",
+                   "rgb(255,100,100)",
+		  ]     
+    }        );
   }
 }
+
+
+
+
+
+
+
+
+
+
 export default withAuthenticator(App);
+
+function getData(){
+    
+    let url = 'https://5bd5x3swz7ch4uxvln2bqscwze0gltjb.lambda-url.eu-west-2.on.aws/'
+
+
+    fetch(url)
+	.then(res => res.json())
+	.then(rows => {
+	    ReactDOM.render(<DeviceData data={rows} />, document.getElementById('device_table'));
+	})
+}
+
+
+
